7fba2cdae8bded861cd7561185baa514e8fb6a5e
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-22 07:38:41 -0300

    Corrigindo Lint

--- helpers/fs/read.js
+++ helpers/fs/read.js
@@ -1,14 +1,12 @@
 const fs = require('fs/promises');
 
 const read = async () => {
-
     try {
-        const data = await fs.readFile('talker.json', {encoding: 'utf-8'});
+        const data = await fs.readFile('talker.json', { encoding: 'utf-8' });
         return JSON.parse(data);
-
     } catch (error) {
         console.log(error.message);
     }
-}
+};
 
 module.exports = read;
--- index.js
+++ index.js
@@ -2,33 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const fsHelps = require('./helpers/fs');
-const vazio = require('./middlewares/validation');
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-app.get('/talker',  async (req, res) => {
-  
-    
+app.get('/talker', async (req, res) => {
     const talker = await fsHelps.read();
-    if(talker.length === 0) {
-      return res.status(HTTP_OK_STATUS).json([])
-    }else{
-      return res.status(HTTP_OK_STATUS).json(talker)
-    }
-   
- 
-    
-  }
+    if (talker.length === 0) {
+      return res.status(HTTP_OK_STATUS).json([]);
+    } 
+      return res.status(HTTP_OK_STATUS).json(talker);
+  });
 
-)
-
-
-
-
-
-/* não remova esse endpoint, e para o avaliador funcionar*/
+/* não remova esse endpoint, e para o avaliador funcionar */
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
