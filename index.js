0392b575205e696b7d0af919bb88ecf68742cd68
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-22 07:31:58 -0300

    Item 01

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fsHelps = require('./helpers/fs');
+const vazio = require('./middlewares/validation');
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+app.get('/talker',  async (req, res) => {
+  
+    
+    const talker = await fsHelps.read();
+    if(talker.length === 0) {
+      return res.status(HTTP_OK_STATUS).json([])
+    }else{
+      return res.status(HTTP_OK_STATUS).json(talker)
+    }
+   
+ 
+    
+  }
+
+)
+
+
+
+
 
 /* não remova esse endpoint, e para o avaliador funcionar*/
 app.get('/', (_request, response) => {
