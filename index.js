c808b7bb409b6e64525ee651a04b20773f825ae1
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-22 14:15:38 -0300

    Item 2

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.get('/talker', async (req, res) => {
       return res.status(HTTP_OK_STATUS).json(talker);
   });
 
+ app.get('/talker/:id', async (req, res) => {
+    const { id: userId } = req.params;
+    const talker = await fsHelps.read();
+    const foundTalker = talker.find((user) => user.id === (+userId));
+  
+    if (!foundTalker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(HTTP_OK_STATUS).json(foundTalker);
+ });
 /* não remova esse endpoint, e para o avaliador funcionar */
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
