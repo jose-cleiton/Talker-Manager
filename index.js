cb8808370446999f2c3709a5fc47fb954dd02b3c
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-06-24 14:41:47 -0300

    Fix: Passando service para arquivo Author

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const generator = require('generate-password');
-const fsHelps = require('./helpers/fs');
-const { 
- 
+const Author = require('./service/Author');
+const {  
   isValidEmail,
   isValidPassword,
   isValidateToken,
@@ -12,7 +10,6 @@ const {
   isValidateTalkField,
   isValidateWatchedAt,
   isValidateRate,
-
 } = require('./middlewares/validation');
 
 const app = express();
@@ -22,32 +19,14 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 /** 1 - Crie o endpoint GET /talker  */
-app.get('/talker', async (req, res) => {
-    const talker = await fsHelps.read();
-    if (talker.length === 0) {
-      return res.status(HTTP_OK_STATUS).json([]);
-    } 
-      return res.status(HTTP_OK_STATUS).json(talker);
-  });
+app.get('/talker', Author.getTalkers);
 
   /**  2 - Crie o endpoint GET /talker/:id */
 
- app.get('/talker/:id', async (req, res) => {
-    const { id: userId } = req.params;
-    const talker = await fsHelps.read();
-    const foundTalker = talker.find((user) => user.id === (+userId));
-  
-    if (!foundTalker) {
-      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-    }
-    return res.status(HTTP_OK_STATUS).json(foundTalker);
- });
+app.get('/talker/:id', Author.getTalkersID);
 
  /** 3 - Crie o endpoint POST /login  */
- app.post('/login', isValidEmail, isValidPassword, (_req, res) => {
-  const token = generator.generate({ length: 16, numbers: true });
-  return res.status(200).json({ token });
- });
+ app.post('/login', isValidEmail, isValidPassword, Author.postLogin);
 
 /** 4 - Adicione as validações para o endpoint /login */
 
@@ -58,26 +37,8 @@ app.get('/talker', async (req, res) => {
   isValidateAge,
   isValidateTalkField,
   isValidateWatchedAt,
-  isValidateRate,  
-   async (req, res) => {    
-    const { name, age, talk: { watchedAt, rate } } = req.body;
-    const talker = await fsHelps.read();
-    talker.push({ 
-      id: talker.length + 1, 
-      name, 
-      age, 
-      talk: { watchedAt, rate } });
-    await fsHelps.write(talker);
-    res.status(201).json({
-      id: talker.length,
-      name,
-      age,
-      talk: {
-        watchedAt,
-        rate,
-      },
-    });
-  }); // endpoint POST /talker
+  isValidateRate, 
+  Author.postTalkers); // endpoint POST /talker
 
   /** 6 - Crie o endpoint PUT /talker/:id */
 
@@ -87,25 +48,8 @@ app.get('/talker', async (req, res) => {
   isValidateAge,
   isValidateTalkField,
   isValidateWatchedAt,
-  isValidateRate,  
-  
-  async (req, res) => {
-    const { id } = req.params;
-    const { name, age, talk: { watchedAt, rate } } = req.body;
-    const talker = await fsHelps.read();
-    const foundTalkerId = talker.findIndex((user) => +user.id === (+id));
-    talker[foundTalkerId] = { name, age, id: +id, talk: { watchedAt, rate } };    
-    await fsHelps.write(talker);
-    res.status(200).json({
-      name,
-      age,
-      id: (+id),
-      talk: {
-        watchedAt,
-        rate,
-      },
-    });
-  });
+  isValidateRate, 
+  Author.putTalkersId); // endpoint PUT /talker/:id 
 
 /* não remova esse endpoint, e para o avaliador funcionar */
 app.get('/', (_request, response) => {
